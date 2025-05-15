410a9e37d58c793dc162ba72e70a686b85ceb50f
Vinicius Custodio <146038328+ViniciusBocchi@users.noreply.github.com>
2025-05-15 18:55:49 -0300

    Delete ocr.py

--- ocr.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-from PIL import Image 
-
-def extrair_texto_receita(imagem_path):
-    imagem = Image.open(imagem_path)
-    texto = pytesseract.image_to_string(imagem, lang='por')
-    return texto
